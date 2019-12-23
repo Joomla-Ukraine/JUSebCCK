cbf59ce7fdeb5cd971237bba50551e00d65394d3
Den <dgm.denys@gmail.com>
2019-12-23 17:49:02 +0200

    Add package data

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # JUSebCCK
 JUSebCCK - library for SEBLOD (fields and tenplates).
+
+## Global functions
+
+### Bind fields
+
+Save data to cck field:
+
+```php
+JUSebCCK\Utils\Data::bind('My new string or data', 'art_title', $config, $fields);
+```
+**art_title — demo field for documentation*
+
+### Clean cache
+
+Clean all cache from `/cache` folder:
+
+```php
+JUSebCCK\Joomla\Cache::clear();
+```
+
+Clean cck cache from `/cache/com_cck` folder, `/cache/com_cck/**` and `/cache/com_home`:
+
+```php
+JUSebCCK\Joomla\Cache::clear(true);
+```
--- script.php
+++ script.php
@@ -1,35 +1,81 @@
 <?php
 /**
- * @package        JUSebCCK
- * @subpackage     Class
+ * JURSSPublisher - Joomla RSS/XML Export System
  *
- * @author         Denys D. Nosov (denys@joomla-ua.org)
- * @copyright (C)  2016-2019 by Denys D. Nosov (https://joomla-ua.org)
- * @license        GNU General Public License version 2 or later
+ * @version       4.x
+ * @package       JURSSPublisher
+ * @author        Denys D. Nosov (denys@joomla-ua.org)
+ * @copyright (C) 2006-2018 by Denys D. Nosov (https://joomla-ua.org)
+ * @license       license.txt
  *
- * @since          1.0
- */
+ **/
 
-/**
- * Installation class to perform additional changes during install/uninstall/update
- *
- * @package  JUSebCCK
- *
- * @since    1.0
- */
-class JUSebCCKInstallerScript
+defined('_JEXEC') or die;
+
+require_once JPATH_SITE . '/libraries/evoapp/vendor/autoload.php';
+
+use Joomla\Archive\Archive;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Version;
+
+error_reporting(0);
+
+jimport('joomla.filesystem.folder');
+jimport('joomla.filesystem.file');
+jimport('joomla.filesystem.archive');
+jimport('joomla.filesystem.path');
+jimport('joomla.error.error');
+
+class Pkg_JUSebCCKInstallerScript
 {
+	protected $message;
+	protected $status;
+	protected $sourcePath;
+	protected $dbSupport = [
+		'mysql',
+		'mysqli',
+		'postgresql',
+		'sqlsrv',
+		'sqlazure'
+	];
+
+	/**
+	 * Pkg_JURSSPublisherInstallerScript constructor.
+	 * @throws \Exception
+	 */
+	public function __construct()
+	{
+		$this->app = Factory::getApplication();
+		$this->db  = Factory::getDbo();
+	}
+
 	/**
 	 * @param $type
 	 * @param $parent
 	 *
-	 * @return void
+	 * @return bool
 	 *
-	 * @since 1.0
+	 * @throws \Exception
+	 * @since 3.0
 	 */
 	public function preflight($type, $parent)
 	{
+		if(version_compare(PHP_VERSION, '5.5', '<'))
+		{
+			$this->app->enqueueMessage(Text::_('PKG_JURSSPUBLISHER_ERROR_INSTALL_PHPVERSION'), 'error');
 
+			return false;
+		}
+
+		if(version_compare(JVERSION, '3.8.0', '<'))
+		{
+			$this->app->enqueueMessage(Text::_('PKG_JURSSPUBLISHER_ERROR_INSTALL_J31'), 'error');
+
+			return false;
+		}
+
+		return true;
 	}
 
 	/**
@@ -72,17 +118,17 @@ class JUSebCCKInstallerScript
 			$path . 'vendor'
 		];
 
-		foreach( $files AS $file )
+		foreach($files AS $file)
 		{
-			if( file_exists($file) )
+			if(file_exists($file))
 			{
 				unlink($file);
 			}
 		}
 
-		foreach( $folders AS $folder )
+		foreach($folders AS $folder)
 		{
-			if( is_dir($folder) )
+			if(is_dir($folder))
 			{
 				$this->unlinkRecursive($folder, 1);
 			}
@@ -99,19 +145,19 @@ class JUSebCCKInstallerScript
 	 */
 	public function unlinkRecursive($dir, $deleteRootToo)
 	{
-		if( !$dh = @opendir($dir) )
+		if(!$dh = @opendir($dir))
 		{
 			return;
 		}
 
-		while( false !== ($obj = readdir($dh)) )
+		while(false !== ($obj = readdir($dh)))
 		{
-			if( $obj === '.' || $obj === '..' )
+			if($obj === '.' || $obj === '..')
 			{
 				continue;
 			}
 
-			if( !@unlink($dir . '/' . $obj) )
+			if(!@unlink($dir . '/' . $obj))
 			{
 				$this->unlinkRecursive($dir . '/' . $obj, true);
 			}
@@ -119,7 +165,7 @@ class JUSebCCKInstallerScript
 
 		closedir($dh);
 
-		if( $deleteRootToo )
+		if($deleteRootToo)
 		{
 			@rmdir($dir);
 		}
